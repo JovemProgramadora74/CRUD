1bcc25105710826961cc02b10e982a252d402d02
Isabela <isabelamagella@gmail.com>
2026-06-15 16:57:08 -0300

    adiciona funcionalidade no botão de curtidas

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Controls;
 using CRUD.Modelos;
 using MySql.Data.MySqlClient;
 
@@ -6,10 +7,13 @@ namespace CRUD;
 
 public partial class Feed : Window
 {
+    private Usuario _usuario;
+    
     public Feed(Usuario usuario)
     {
         InitializeComponent();
         CarregarPosts_QuandoIniciar();
+        _usuario = usuario;
     }
 
     private void CarregarPosts_QuandoIniciar()
@@ -55,6 +59,50 @@ public partial class Feed : Window
         catch (Exception e)
         {
             Console.WriteLine(e);
+        }
+    }
+
+    private void BtnCurtir_OnClick(object sender, RoutedEventArgs e)
+    {
+        var botao = (Button)sender;
+        var postagem = (Postagem)botao.Tag;
+
+        var query = "SELECT 1 FROM curtidas_postagens WHERE usuario_id = @usuario AND postagem_id = @postagem";
+        
+        using var conexao = new MySqlConnection(App.StringConexao);
+        
+        using var comando = new MySqlCommand(query, conexao);
+        comando.Parameters.AddWithValue("@usuario", _usuario.Id);
+        comando.Parameters.AddWithValue("@postagem", postagem.Id);
+        
+        try
+        {
+            conexao.Open();
+            var leitor = comando.ExecuteReader();
+            string acao;
+            
+            if (leitor.HasRows)
+            {
+                query = "DELETE FROM curtidas_postagens WHERE usuario_id = @usuario AND postagem_id = @postagem";
+                acao = "descurtir";
+            }
+            else
+            {
+                query = "INSERT INTO curtidas_postagens (usuario_id, postagem_id) VALUES (@usuario, @postagem)";
+                acao = "curtir";
+            }
+
+            conexao.Close();
+            comando.CommandText = query;
+            conexao.Open();
+            var linhasAfetadas = comando.ExecuteNonQuery();
+            if (linhasAfetadas == 0) throw new Exception($"Erro ao {acao} postagem!");
+            
+            
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show(exception.Message);
         }
     }
 }
