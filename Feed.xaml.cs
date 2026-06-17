27d4d1633e9bdfb97685db189fbec8c1c785676a
Isabela <isabelamagella@gmail.com>
2026-06-17 14:59:25 -0300

    resgata posts curtidos pelo usuário

--- Feed.xaml.cs
+++ Feed.xaml.cs
@@ -11,9 +11,9 @@ public partial class Feed : Window
     
     public Feed(Usuario usuario)
     {
+        _usuario = usuario;
         InitializeComponent();
         CarregarPosts_QuandoIniciar();
-        _usuario = usuario;
     }
 
     private void CarregarPosts_QuandoIniciar()
@@ -21,10 +21,11 @@ public partial class Feed : Window
         List<Postagem> listaPostagens = [];
 
         const string query =
-            "SELECT p.id, p.conteudo, p.curtidas, p.postado_em, u.nome, u.username FROM postagens p INNER JOIN usuarios u ON p.usuario_id = u.id ORDER BY p.postado_em DESC";
+            "SELECT p.id, p.conteudo, p.curtidas, p.postado_em, u.nome, u.username, IF(cp.usuario_id IS NOT NULL, TRUE, FALSE) AS curtido FROM postagens p INNER JOIN usuarios u ON p.usuario_id = u.id LEFT JOIN curtidas_postagens cp ON cp.postagem_id = p.id AND cp.usuario_id = @usuario_id ORDER BY p.postado_em DESC";
         using var conexao = new MySqlConnection(App.StringConexao);
 
         using var comando = new MySqlCommand(query, conexao);
+        comando.Parameters.AddWithValue("@usuario_id", _usuario.Id);
 
         try
         {
@@ -44,6 +45,7 @@ public partial class Feed : Window
                     Conteudo = leitor.GetString("conteudo"),
                     Curtidas = leitor.GetInt32("curtidas"),
                     Postado_em = leitor.GetDateTime("postado_em"),
+                    FoiCurtido =  leitor.GetBoolean("curtido"),
                     Usuario = new Usuario
                     {
                         Nome = leitor.GetString("nome"),
